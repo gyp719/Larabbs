f9b2b6bae31f55cd1b28e2e71fdca586089d32a5
gyp719 <836230540@qq.com>
2022-11-03 19:19:07 +0900

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,16 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, MustVerifyEmailTrait;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -26,22 +22,17 @@ class User extends Authenticatable implements MustVerifyEmail
         'avatar',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function topics(): HasMany
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,7 +7,7 @@
   <div class="row">
 
     <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
-      <div class="card ">
+      <div class="card">
         <img class="card-img-top"
              src="{{ $user->avatar }}"
              alt="{{ $user->name }}">
@@ -21,7 +21,7 @@
       </div>
     </div>
     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-      <div class="card ">
+      <div class="card">
         <div class="card-body">
           <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
         </div>
@@ -29,9 +29,13 @@
       <hr>
 
       {{-- 用户发布的内容 --}}
-      <div class="card ">
+      <div class="card">
         <div class="card-body">
-          暂无数据 ~_~
+          <ul class="nav nav-tabs">
+            <li class="nav-item"><a class="nav-link active bg-transparent" href="#">Ta 的话题</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Ta 的回复</a></li>
+          </ul>
+          @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
         </div>
       </div>
 
