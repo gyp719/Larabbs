2c21f215726a9a4d08a00fa6dd1366cbb5514472
gyp719 <836230540@qq.com>
2022-11-07 00:58:17 +0900

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,6 +11,8 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body);
+
         $topic->excerpt = make_excerpt($topic->body);
     }
 
