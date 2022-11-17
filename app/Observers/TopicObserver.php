fbe5ccc6318d6ec9e37378f5f10329ca6451da87
gyp719 <836230540@qq.com>
2022-11-17 20:21:03 +0900

    修改标题 slug更新

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -19,7 +19,8 @@ class TopicObserver
         $topic->excerpt = make_excerpt($topic->body);
 
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-        if ( ! $topic->slug) {
+        if (!$topic->slug || $topic->isDirty('title')) {
+
             $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
         }
     }
