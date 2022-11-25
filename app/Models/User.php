de815a4c6640784ab9f9c777f2cd8a2d39af025f
gyp719 <836230540@qq.com>
2022-11-25 20:16:22 +0900

    用户最后活跃时间

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable implements MustVerifyEmail
 {
     use Traits\ActiveUserHelper;
+    use Traits\LastActivedAtHelper;
 
     use HasRoles;
 
